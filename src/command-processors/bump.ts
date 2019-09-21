7d6f645d8edb417c049030905c21da8ec5737de5
pachet <pachet@burninggarden.com>
2019-09-21 09:03:47 -0400

    Show realtime stdout for bump command

--- src/command-processors/bump.ts
+++ src/command-processors/bump.ts
@@ -4,9 +4,18 @@ import CommandProcessor from 'command-processor';
 class BumpCommandProcessor extends CommandProcessor {
 
 	protected run(): Promise<any> {
-		ChildProcess.execSync('npm version patch');
-		ChildProcess.execSync('git push origin master');
-		ChildProcess.execSync('npm publish');
+		ChildProcess.execSync('npm version patch', {
+			stdio: 'inherit'
+		});
+
+		ChildProcess.execSync('git push origin master', {
+			stdio: 'inherit'
+		});
+
+		ChildProcess.execSync('npm publish', {
+			stdio: 'inherit'
+		});
+
 		return Promise.resolve();
 	}
 
